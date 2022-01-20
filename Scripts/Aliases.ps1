af57728653c4767e03d3d38ceabbc232297f17be
petrosAth <develop@athanasoulis.me>
2022-01-20 02:27:10 +0200

    Add aliases for WSL
    Add aliases for running neovim through wsl. Also add aliases for arch and manjaro.

--- Scripts/Aliases.ps1
+++ Scripts/Aliases.ps1
@@ -3,6 +3,23 @@
 # Alias for sending items to recycle bin
 Set-Alias -Name trash -Value Remove-ItemSafely
 
+if ($IsWindows) {
+    # Alias for running nvim through wsl
+    Function nvim {
+        C:\Users\petrosAth\scoop\apps\archwsl\21.8.28.0\Arch.exe runp nvim $args[0]
+    }
+
+    # Alias for Arch WSL
+    Function arch {
+        C:\Users\petrosAth\scoop\apps\archwsl\21.8.28.0\Arch.exe
+    }
+
+    # Alias for Manjaro WSL
+    Function manjaro {
+        C:\Users\petrosAth\scoop\apps\manjarowsl\20220116\Manjaro.exe
+    }
+}
+
 # Alias for neovide
 Function nvid {
     param(
