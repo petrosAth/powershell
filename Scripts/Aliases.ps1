1b607f3c1a452cc98454cca1bf07af72410356c5
petrosAth <develop@athanasoulis.me>
2022-01-24 17:06:33 +0200

    Simplify wsl nvim alias path

--- Scripts/Aliases.ps1
+++ Scripts/Aliases.ps1
@@ -6,17 +6,7 @@ Set-Alias -Name trash -Value Remove-ItemSafely
 if ($IsWindows) {
     # Alias for running nvim through wsl
     Function nvim {
-        C:\Users\petrosAth\scoop\apps\archwsl\21.8.28.0\Arch.exe runp nvim $args[0]
-    }
-
-    # Alias for Arch WSL
-    Function arch {
-        C:\Users\petrosAth\scoop\apps\archwsl\21.8.28.0\Arch.exe
-    }
-
-    # Alias for Manjaro WSL
-    Function manjaro {
-        C:\Users\petrosAth\scoop\apps\manjarowsl\20220116\Manjaro.exe
+        Arch.exe runp nvim $args[0]
     }
 }
 
