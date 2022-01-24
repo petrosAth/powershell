9e3ba91666abb937cc4202b3e41d763cf924b450
petrosAth <develop@athanasoulis.me>
2022-01-24 18:24:53 +0200

    Start Alacritty when WSL nvim is called from Windows Terminal

--- Scripts/Aliases.ps1
+++ Scripts/Aliases.ps1
@@ -3,22 +3,29 @@
 # Alias for sending items to recycle bin
 Set-Alias -Name trash -Value Remove-ItemSafely
 
+# Alias for running nvim through wsl only under Windows
 if ($IsWindows) {
-    # Alias for running nvim through wsl
     Function nvim {
-        Arch.exe runp nvim $args[0]
+        # If we are in Windows Terminal start Alacritty
+        if ($env:WT_SESSION) {
+            alacritty -e Arch.exe runp nvim $args[0]
+        }
+        else {
+            Arch.exe runp nvim $args[0]
+        }
     }
 }
 
-# Alias for neovide
-Function nvid {
-    param(
-        [string] $File
-    )
 
-    # neovide --geometry=120x40 $File
-    neovide --maximized $File
-}
+# Alias for neovide
+# Function nvid {
+#     param(
+#         [string] $File
+#     )
+#
+#     # neovide --geometry=120x40 $File
+#     neovide --maximized $File
+# }
 
 # Debugging #-------------------------------------------------------------------
 if ($Debugging) {
