b939d14b4c96d0d69ad78131583c112ede0f427e
petrosAth <develop@athanasoulis.me>
2022-05-11 00:07:39 +0300

    Add ohmyposh transient prompt

--- Scripts/Styling/oh-my-posh.ps1
+++ Scripts/Styling/oh-my-posh.ps1
@@ -3,9 +3,11 @@
 # Load oh-my-posh and set theme
 if ($IsWindows) {
     oh-my-posh --init --shell pwsh --config $dotfilesDir\ohmyposh\$DefaultTheme.omp.json | Invoke-Expression
+    Enable-PoshTransientPrompt
 }
 elseif ($IsLinux) {
     oh-my-posh --init --shell pwsh --config $dotfilesDir/ohmyposh/$DefaultTheme.omp.json | Invoke-Expression
+    Enable-PoshTransientPrompt
 }
 else {
 		Write-Error -Message "oh-my-posh.ps1 - Not supported operating system"
